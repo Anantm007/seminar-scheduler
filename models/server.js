db07e264bf7fc838bdf0f1d7ab0525dc634b6ff8
Anant Mathur <anant.mathur007@gmail.com>
2020-03-11 19:53:08 +0530

    Initial Commit

---
+++ models/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const app = express();
+
+// Middleware utilities
+const bodyParser = require("body-parser");
+
+const path = require('path'); 
+
+// Mongoose 
+const mongoose = require('mongoose');
+
+// Config variables
+require('dotenv').config();
+
+
+//Connecting to the database
+mongoose.promise = global.Promise;
+mongoose.connect(process.env.MongoURI,{useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true}, (err,db)=> {
+    if(err)
+    console.log(err);
+
+    else
+    console.log('Database Connected...');
+});
+
+
+// Getting data in json format
+app.use(bodyParser.json())
+
+// Test route
+app.get("/api", (req, res) => {
+    res.json({
+        message: "API running"
+    });
+})
+
+// Mounting the routes
+//app.use('/api/society', require('./routes/society.js'));
+
+
+// Serve Static Assets in production
+if(process.env.NODE_ENV === 'production')
+{
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    })
+}
+
+
+// Starting the server
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log(`Server started on port ${process.env.PORT || 3000}`);
+});
