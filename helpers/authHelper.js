54feeeacce5dfcba8ea31c331635b510145b7ea8
Ankur Gupta <gupta.ankur255@gmail.com>
2020-03-12 20:03:33 +0530

    Added Signin Route

---
+++ helpers/authHelper.js
@@ -0,0 +1,23 @@
+const jwt=require('jsonwebtoken')
+const Society=require('../models/society')
+
+const auth=async(req,res,next)=>{
+    try{
+        const token=req.header('Authorization').replace('Bearer ','')
+        const decoded=jwt.verify(token,process.env.JWTSECRET)
+        const society=await Society.findOne({
+            _id: decoded._id,
+            'tokens.token': token
+        })
+        if(!society){
+            throw new Error()
+        }
+        req.token=token
+        req.society=society
+        next()
+    }catch(e){
+        res.status(401).send({error: 'Please Authenticate'})
+    }
+}
+
+module.exports=auth
