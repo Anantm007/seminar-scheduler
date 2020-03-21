7ebc81d07cdfbb29405459cdd10134d0c902d3a3
Ankur Gupta <gupta.ankur255@gmail.com>
2020-03-21 12:48:31 +0530

    Added Signin Page

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,10 +1,12 @@
 import React, {Fragment} from 'react';
 import './App.css';
+import Signin from '../components/admin/signin/signin'
 
 const App = () => {
   return (
     <Fragment>
       <h1>Seminar Schedular MSIT</h1>
+      <Signin />
     </Fragment>
   );
 }
