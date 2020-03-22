51e2ba3d679d2e638a280906cfa749dd6e0d882b
Anant Mathur <anant.mathur007@gmail.com>
2020-03-22 22:38:11 +0530

    Landing, notFound and update routing

---
+++ client/src/components/layout/notFound/Notfound.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div style={{marginTop: '5rem', textAlign: 'center'}}>
+            <h1 className="x-large text-primary">
+                Error 404 :( <br/><br/>
+                <i className="fas fa-exclamation-triangle"></i>Page Not Found
+                <br/>
+            </h1>
+
+            <p className="large">Sorry, this page does not exist</p>
+            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br/>
+
+        </div>
+    )
+}
+
+export default NotFound
