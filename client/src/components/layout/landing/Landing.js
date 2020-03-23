ec083e9fe691b16bdf39147384454469ac423f13
Ankur Gupta <gupta.ankur255@gmail.com>
2020-03-23 13:37:14 +0530

    Added Navbar template

--- client/src/components/layout/landing/Landing.js
+++ client/src/components/layout/landing/Landing.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import Navigation from '../navbar/navbar';
 
 const NotFound = () => {
     return (
-        <div style={{marginTop: '5rem', textAlign: 'center'}}>
-            <h1 className="x-large text-primary">
-                Seminar Scheduler MSIT<br/><br/>
-                <br/>
-            </h1>
-
-
+        <div>
+            <Navigation />
+            <div style={{marginTop: '5rem', textAlign: 'center'}}>
+                <h1 className="x-large text-primary">
+                    Seminar Scheduler MSIT<br/><br/>
+                    <br/>
+                </h1>
+            </div>
         </div>
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { BrowserRouter } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
