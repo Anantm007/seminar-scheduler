e2487372b8a3e740d6945027bd6d0775e0c9191b
Ankur Gupta <gupta.ankur255@gmail.com>
2020-04-04 23:16:21 +0530

    Added Improvements

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
